b313a70b7be36bed6fef3777b9a68ffe78330490
fbueno99 <fb99@protonmail.com>
2018-10-29 22:19:13 -0200

    firts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ with open('portfolio/SECRET_KEY.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["porttfolio.pythonanywhere.com"]
 
 
 # Application definition
--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -6,11 +6,29 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
+'''
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
+application = get_wsgi_application()
+'''
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
+import os
+import sys
+#
+## assuming your django settings file is at '/home/porttfolio/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/porttfolio/mysite/manage.py'
+path = '/home/user/ddjangoo/porttfolio/portfolio/portfolio' #ddjangoo\porttfolio\portfolio\portfolio
+if path not in sys.path:
+    sys.path.append(path)
+#
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+#
+## then:
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
